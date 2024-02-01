6ddd689a13558d7147271693d9c1702d76382366
Archit Aggarwal <155532480+Archit-24@users.noreply.github.com>
2024-02-01 17:27:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ option = webdriver.ChromeOptions()
 option.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=option)
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3792947356&f_AL=true&geoId=102713980&keywords=Python%20Developer&location=India&origin=JOB_SEARCH_PAGE_LOCATION_HISTORY&refresh=true")
+driver.get()
 
 
 
